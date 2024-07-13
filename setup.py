49ce4c11aa303f6e5b0a1a23e810049fe6dd2ade
Adarsh Singh <adarsharunsingh7@gmail.com>
2024-07-13 16:01:10 +0530

    updated code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+
+name ="Xray",
+version ="0.0.1",
+author ="Adarsh Singh",
+author_email="adarsharunsingh7@gmail.com",
+install_requires=[],
+package =find_packages()
+)
