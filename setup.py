dfa4335e7fb3fe24bae2fa4d2437de649fee2e03
Adarsh Singh <adarsharunsingh7@gmail.com>
2024-07-13 16:36:13 +0530

    change

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
+
+with open("requirements_dev.txt") as f:
+    required = f.read().splitlines()
 
 setup(
-
-name ="Xray",
-version ="0.0.1",
-author ="Adarsh Singh",
-author_email="adarsharunsingh7@gmail.com",
-install_requires=[],
-package =find_packages()
+    name="Xray",
+    version="0.0.1",
+    author="Adarsh Singh",
+    author_email="adarsharunsingh7@gmail.com",
+    install_requires=required,
+    packages=find_packages()
 )
